57cf5ff9a44d27442a1141a2711e0008749915ee
Tawhid Jony <tawhid.jony.jhm@gmail.com>
2021-07-08 18:43:58 +0600

    create

--- app/Controllers/Http/StudentsController.ts
+++ app/Controllers/Http/StudentsController.ts
@@ -1,4 +1,25 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Student from 'App/Models/Student'
 
 export default class StudentsController {
+
+
+
+ public async store({request, response}:HttpContextContract){
+  const req = await request.all()
+  const student = new Student()
+        student.name = req.name
+        student.email = req.name
+        student.phone = req.name
+        student.address = req.address
+
+        await student.save()
+
+        return response.redirect('/student')
+
+ }
+
+
+
 }
